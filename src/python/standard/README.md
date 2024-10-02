538df73c0c9e0db85b1de71615b190fb4f0c4eec
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2024-10-02 09:13:21 +0200

    Implement FizzBuzz in a standard way in python

---
+++ src/python/standard/README.md
@@ -0,0 +1,19 @@
+# FizzBuzz in python
+This is a straight forward python implementation of FizzBuzz
+
+## Development
+Make sure to create an venv directory with the following command
+
+```plain
+python3 -m venv .venv
+```
+
+And source the correct file for your environment. E.g.
+
+```plain
+source .venv/bin/activate
+```
+
+`main.py` is a selfcontained python program that tests the FizzBuzz
+implementation. If you want to develop you can execute `tdd`. This is a fish
+shell script that wathes the `main.py` and on writes runs it.
---
+++ src/python/standard/main.py
@@ -0,0 +1,36 @@
+def fizzbuzz(n):
+    if n % 15 == 0:
+        return "FizzBuzz"
+    if n % 5 == 0:
+        return "Buzz"
+    if n % 3 == 0:
+        return "Fizz"
+    return f"{n}"
+
+class FizzBuzzTest:
+    def __init__(self, n):
+        self.n = n
+
+    def should_be(self, expected):
+        assert fizzbuzz(self.n) == expected, f"fizzbuzz({self.n}) should equal \"{expected}\" but was \"{fizzbuzz(self.n)}\""
+        
+def fizzbuzz_of(n):
+    return FizzBuzzTest(n)
+
+if __name__ == "__main__":
+    fizzbuzz_of(1).should_be("1")
+    fizzbuzz_of(2).should_be("2")
+    fizzbuzz_of(3).should_be("Fizz")
+    fizzbuzz_of(4).should_be("4")
+    fizzbuzz_of(5).should_be("Buzz")
+    fizzbuzz_of(6).should_be("Fizz")
+    fizzbuzz_of(7).should_be("7")
+    fizzbuzz_of(8).should_be("8")
+    fizzbuzz_of(9).should_be("Fizz")
+    fizzbuzz_of(10).should_be("Buzz")
+    fizzbuzz_of(11).should_be("11")
+    fizzbuzz_of(12).should_be("Fizz")
+    fizzbuzz_of(13).should_be("13")
+    fizzbuzz_of(14).should_be("14")
+    fizzbuzz_of(15).should_be("FizzBuzz")
+
