b6bc8642b1714b3fdd82608dcfe7a8b592f62a02
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2024-10-07 07:39:33 +0200

    Create a generic test runner

---
+++ src/test/kotlin/nl/fifthpostulate/FizzBuzzTest.kt
@@ -0,0 +1,48 @@
+package nl.fifthpostulate
+
+import nl.fifthpostulate.radix.Radix
+import nl.fifthpostulate.standard.Standard
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FizzBuzzTest {
+    lateinit var fizzbuzz : FizzBuzz
+
+    @BeforeEach
+    fun createFizzBuzz() {
+        fizzbuzz = Radix()
+    }
+
+    @Test
+    fun verify_fizzbuzz() {
+        listOf(Standard(), Radix()).forEach { subject ->
+            with(subject) {
+                verify().fizzbuzzOf(1).shouldEqual("1")
+                verify().fizzbuzzOf(2).shouldEqual("2")
+                verify().fizzbuzzOf(3).shouldEqual("Fizz")
+                verify().fizzbuzzOf(5).shouldEqual("Buzz")
+                verify().fizzbuzzOf(6).shouldEqual("Fizz")
+                verify().fizzbuzzOf(10).shouldEqual("Buzz")
+                verify().fizzbuzzOf(15).shouldEqual("FizzBuzz")
+            }
+
+        }
+    }
+}
+
+fun FizzBuzz.verify(): FizzBuzzTestCase =
+    FizzBuzzTestCase(this)
+
+class FizzBuzzTestCase(val fizzbuzz: FizzBuzz) {
+    fun fizzbuzzOf(n : Int): FizzBuzzExpectation =
+        FizzBuzzExpectation(fizzbuzz, n)
+
+}
+
+class FizzBuzzExpectation(val fizzbuzz: FizzBuzz, val n : Int) {
+    fun shouldEqual(expected: String) {
+        val actual = fizzbuzz.of(n)
+        assertEquals(actual, expected, "fizzbuzz.of($n) should equal \"$expected\", but was \"$actual\"")
+    }
+}
