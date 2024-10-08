1aac0ba38e6a770343642f8df373c363b232cfef
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2024-10-08 07:43:54 +0200

    Find the alternating sum residue

--- src/main/kotlin/nl/fifthpostulate/castingnines/CastingNines.kt
+++ src/main/kotlin/nl/fifthpostulate/castingnines/CastingNines.kt
@@ -1,11 +1,12 @@
 package nl.fifthpostulate.castingnines
 
 import nl.fifthpostulate.FizzBuzz
+import kotlin.math.absoluteValue
 
 class CastingNines : FizzBuzz {
     override fun of(n: Int): String {
-        val divisibility3 = alternatingDigitSum(2, 1, n) % 3 == 0
-        val divisibility5 = alternatingDigitSum(4, 1, n) % 5 == 0
+        val divisibility3 = divisibleBy3(n)
+        val divisibility5 = divisibleBy5(n)
         if (divisibility5 && divisibility3) {
             return "FizzBuzz"
         }
@@ -18,6 +19,20 @@ class CastingNines : FizzBuzz {
         return n.toString()
     }
 }
+
+fun divisibleBy3(n : Int): Boolean =
+    alternatingDigitSumResidue(2, n) == 0
+
+fun divisibleBy5(n : Int): Boolean =
+    alternatingDigitSumResidue(4, n) == 0
+
+fun alternatingDigitSumResidue(base: Int, n: Int): Int =
+    if (n.absoluteValue < base) {
+        n
+    } else {
+        val m = alternatingDigitSum(base, 1, n)
+        alternatingDigitSumResidue(base, m)
+    }
 
 fun alternatingDigitSum(base: Int, multiplier: Int, subject: Int): Int {
     val quotient = subject / base
